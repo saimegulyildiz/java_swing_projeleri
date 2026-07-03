2988d1ec111c964d2b0fc46b8249cdf43ea2e5a0
saimegulyildiz <yildizsaimegul@gmail.com>
2026-07-03 21:42:43 +0300

    Add files via upload

---
+++ Yazalistirmalari.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yazalistirmalari;
+
+/**
+ *
+ * @author acer
+ */
+public class Yazalistirmalari {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
